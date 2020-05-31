3d2891244fcf6e80a3b25db0c529bdb6099d7c4a
CCortinas <crcortinas@gmail.com>
2020-05-30 19:39:44 -0500

    update

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
-const http = require('http');
-const express = require('express');
-const socketio = require('socket.io');
-const randomColor = require('randomcolor');
-const createBoard = require('./create-board');
+const http = require("http");
+const express = require("express");
+const socketio = require("socket.io");
+const randomColor = require("randomcolor");
+const createBoard = require("./create-board");
 
 const app = express();
 const clientPath = `${__dirname}/../client`;
@@ -13,24 +13,26 @@ app.use(express.static(clientPath));
 
 const server = http.createServer(app);
 const io = socketio(server);
-const { makeTurn, getBoard } = createBoard(20);
+const { makeTurn, getBoard, clear } = createBoard(20);
 
-io.on('connection', (sock) => {
+io.on("connection", (sock) => {
   const color = randomColor();
 
-  sock.on('message', (text) => io.emit('message', text));
-  sock.on('turn', ({ x, y }) => {
-    io.emit('turn', { x, y, color });
+  sock.on("message", (text) => io.emit("message", text));
+  sock.on("turn", ({ x, y }) => {
+    io.emit("turn", { x, y, color });
     makeTurn(x, y, color);
   });
 
-  sock.emit('board', getBoard());
+  sock.emit("board", getBoard());
 });
 
-server.on('error', (err) => {
-  console.error('Server error:', err);
+server.on("error", (err) => {
+  console.error("Server error:", err);
 });
 
-server.listen(8080, () => {
-  console.log('server started on 8080');
+const PORT = process.env.PORT || 8080;
+
+server.listen(PORT, () => {
+  console.log("server started on ", PORT);
 });
